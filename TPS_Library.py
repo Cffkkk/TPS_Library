9376f0f4476b35ee44041f0803c098b9a191cb54
Cffkkk <2018303060089@whu.edu.cn>
2024-09-09 00:54:29 +0800

    Add files via upload

--- TPS_Library.py
+++ TPS_Library.py
@@ -1,10 +1,13 @@
 import streamlit as st
-import pandas as pd
 import datetime
 from st_aggrid import AgGrid, DataReturnMode, GridUpdateMode, GridOptionsBuilder
 from st_aggrid.shared import JsCode
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
 
-st.set_page_config(layout='wide')
+st.set_page_config(layout='wide',page_icon=":bar_chart:")
+
 
 st.title("Terpene Synthase-standard Library")
 
@@ -20,8 +23,12 @@ elif 10 <= current_hour < 14:
 else:
     st.write("### It's late, time to rest!")
 
-file_path = 'output.csv'
-df = pd.read_csv(file_path)
+@st.cache_data
+def data_create():
+    file_path = 'LLLT_.csv'
+    data = pd.read_csv(file_path)
+    return data
+df = data_create()
 
 # 定义AG-Grid的列配置
 gb = GridOptionsBuilder.from_dataframe(df)
@@ -30,6 +37,7 @@ quick_filter_text = st.text_input(r"$\textsf{\Large search}$")
 gb.configure_grid_options(
     quickFilterText = quick_filter_text
 )
+
 gb.configure_default_column()
 gb.configure_column("ComPound No", filter="agTextColumnFilter")
 gb.configure_column("Gene No", filter="agTextColumnFilter")
@@ -38,17 +46,31 @@ gb.configure_column("Identification method", filter="agTextColumnFilter")
 gb.configure_column("TPS original information", filter="agTextColumnFilter")
 gb.configure_column("RT", filter="agTextColumnFilter")
 gb.configure_column("Formula", filter="agTextColumnFilter")
-gb.configure_column("Speices", filter="agTextColumnFilter")
+gb.configure_column("Species", filter="agTextColumnFilter")
 gb.configure_column("Resource", filter="agTextColumnFilter")
 gb.configure_column("Main or By Product", filter="agTextColumnFilter")
 gb.configure_column("Link", filter="agTextColumnFilter")
 
-# 自定义cellRenderer来显示超链接
 cell_renderer = JsCode("""
     class LinkCellRenderer {
       init(params) {
         this.eGui = document.createElement('div');
-        this.eGui.innerHTML = params.value;
+        if (params.value) { // 检查是否有值
+          // 检查是否是超链接格式
+          let isLink = params.value.match(/<a href="([^"]+)" target="_blank">([^<]+)<\/a>/);
+          if (isLink) {
+            let href = isLink[1];
+            let linkText = isLink[2];
+            if (!href.startsWith('http')) { // 检查是否是完整的 URL
+              href = window.location.origin + '/' + href; // 转换为绝对路径
+            }
+            this.eGui.innerHTML = `<a href="${href}" target="_blank">${linkText}</a>`;
+          } else {
+            this.eGui.innerHTML = params.value; // 直接显示值
+          }
+        } else {
+          this.eGui.innerHTML = ''; // 如果没有值，显示 'No link'
+        }
         this.eGui.style.cursor = 'pointer';
       }
       getGui() {
@@ -63,7 +85,17 @@ gb.configure_column(
     headerName="UniProt ID or NCBI ID",
     cellRenderer=cell_renderer
 )
-
+gb.configure_column(
+    "Gene No",
+    headerName="Gene No",
+    cellRenderer=cell_renderer
+)
+gb.configure_column(
+    "ComPound No",
+    headerName="ComPound No",
+    cellRenderer=cell_renderer
+)
+df['se'] = None
 # 创建AG-Grid表格
 AgGrid(df,
     gridOptions=gb.build(),
@@ -77,3 +109,54 @@ st.write("1. Ye, Z., et al., CouPling cell growth and biochemical Pathway induct
          "3. Quin, M.B., S.N. Michel, and C. Schmidt‐Dannert, Moonlighting Metals: Insights into Regulation of Cyclization Pathways in Fungal Δ6‐Protoilludene SesquiterPene Synthases. ChemBioChem, 2015. 16(15): P. 2191-2199.\n"
          "4. Chen, R., et al., Characterization of tremulane sesquiterPene synthase from the basidiomycete IrPex lacteus. Organic Letters, 2022. 24(31): P. 5669-5673.\n"
          "5. Burkhardt, I., et al., Mechanistic characterization of three sesquiterPene synthases from the termite-associated fungus Termitomyces. Organic & Biomolecular Chemistry, 2019. 17(13): 3348-3355.")
+
+# 横向条形图
+sales_by_product_line = (
+    df.groupby(by=["Identification method"]).count()[["ComPound No"]]
+)
+color_map = {
+    "Standard": "#386795",
+    "Others": "#73BCD5",
+    "NMR": "#FFD06E",
+    "Commercial standard": "#E86254"
+}
+fig_product_sales = px.bar(
+    sales_by_product_line,
+    x="ComPound No",
+    y=sales_by_product_line.index,
+    orientation="h",
+    color=sales_by_product_line.index,
+    color_discrete_map=color_map,
+    title="<b>Identification method of Compounds</b>"
+)
+fig_product_sales.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False))
+)
+
+las = df.groupby(df['Resource']).size()
+las.sort_values(ascending=True,inplace=True)
+layout = go.Layout(
+    title = '<b>Resource of Compounds</b>',
+    barmode='stack'
+)
+colors = ['#386795', '#73BCD5', '#FFD06E', '#E86254']
+fig_price_sales = go.Figure(data=[go.Pie(labels=las.index, hole = 0.7,values=las.values,hoverinfo = "label+percent",marker=dict(colors=colors))],layout=layout)
+fig_price_sales.update_layout(
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False)),
+)
+left_column, right_column = st.columns(2)
+right_column.plotly_chart(fig_price_sales, use_container_width=True)
+left_column.plotly_chart(fig_product_sales, use_container_width=True)
+
+# 隐藏streamlit默认格式信息
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
